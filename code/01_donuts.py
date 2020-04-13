967d56fbe45621e2022aaeb4ab8120158794094b
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-04-13 09:47:23 -0300

    minha primeira implementacao 01-donuts

--- code/01_donuts.py
+++ code/01_donuts.py
@@ -10,8 +10,11 @@ e donuts(23) retorna 'Number of donuts: many'
 
 
 def donuts(count):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if count < 10:
+        message = 'Number of donuts: ' + str(count)
+    else:
+        message = 'Number of donuts: many'
+    return message
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
