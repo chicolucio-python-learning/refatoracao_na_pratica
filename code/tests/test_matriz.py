cc02d5dc8956426d9ad0abb3b23c25f1edc9d60a
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-05-15 14:14:18 -0300

    Teste para funcao clean_array

--- code/tests/test_matriz.py
+++ code/tests/test_matriz.py
@@ -1,9 +1,22 @@
-from matriz import create_array, string
+from matriz import create_array, string, clean_array
 from textwrap import dedent
 
 
 def test_create():
     board = create_array(['4', '5'])
+    assert string(board) == dedent(
+        '''\
+        OOOO
+        OOOO
+        OOOO
+        OOOO
+        OOOO'''
+    )
+
+
+def test_clean():
+    board = create_array(['4', '5'], 'X')
+    board = clean_array(board)
     assert string(board) == dedent(
         '''\
         OOOO
