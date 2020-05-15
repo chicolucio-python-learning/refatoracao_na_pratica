5c1a799192d82e3e48ff7c748128d7305d9748d6
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-05-15 14:11:05 -0300

    Organizacao das docstrings e reoordenamento das funcoes

--- code/matriz.py
+++ code/matriz.py
@@ -2,54 +2,6 @@
 
 BLANK = "O"
 
-# TODO: Mover o read_sequence para próximo do main.
-# TODO: Mover o comentário para uma docstring
-# TODO: Melhorar o nome read_sequence, pois na verdade ela lê o comando.
-# TODO: Separar as responsabilidades do read_sequence para garantir a leitura de um comando válido e
-#       validar o comando em si.
-# TODO: Verificar se faz sentido ter um parser único para o comando validado, eliminando List[str] e usando algo
-#       de mais alto nível.
-
-
-def read_sequence():  # Read and validate a sequence of commands.
-    # TODO: Melhorar o nome charValid para algo mais semântico.
-    # TODO: Remover os parêntesis desnecessários.
-    charValid = ("ICLVHKFSX")
-    # TODO: Melhorar o nome sqc. Sugestão: cmd
-    # TODO: Separar a obtenção do comando, do tratamento do comando.
-    sqc = input("Digite um comando: ").upper()
-    sqc = sqc.split()
-
-    for char in charValid:
-        # TODO: Verificar a real necessidade desse "and".
-        if char == sqc[0] and not "":
-            return sqc
-    else:
-        print("\nComando Inválido!\n")
-        return sqc
-    # TODO: Implementar um único return.
-
-# TODO: Mover o comentário para docstring.
-# TODO: Extrair o print do print_board.
-# TODO: print_board deveria ser "board_como_string" e caso o board for uma instância de uma classe nossa, implementar o
-#       __str__.
-
-
-def string(board):
-    return '\n'.join((''.join(row) for row in board))
-
-
-def print_board(board):  # Print the Board.
-    print("\n")
-    print(string(board))
-    print("\n")
-
-# TODO: Mover o comentário para um docstring.
-# TODO: Expandir o argument cmd para contemplar as partes específicas.
-# TODO: Mudar "col" e "line" para "width" e "height".
-# TODO: Idealmente width e height já deveriam chegar como int.
-# TODO: Mudar o nome create_array para algo ligado ao board.
-
 
 def create_array(cmd, value=BLANK):
     """Create a array - 'I' Command."""
@@ -57,9 +9,20 @@ def create_array(cmd, value=BLANK):
     return [[value] * col for _ in range(row)]
 
 
-# TODO: Mover o comentário para docstring.
-# TODO: Renomar clean_array para algo melhor.
-def clean_array(board):  # Clean a array - 'C' Command.
+def string(board):
+    return '\n'.join((''.join(row) for row in board))
+
+
+def print_board(board):
+    """Print the Board."""
+    print("\n")
+    print(string(board))
+    print("\n")
+
+
+def clean_array(board):
+    # TODO: Renomar clean_array para algo melhor.
+    """Clean a array - 'C' Command."""
     # TODO: Extrair len(board) e len(board[line]) para variáveis.
     # TODO: Remover o "0" do range.
     for line in range(0, len(board)):
@@ -67,26 +30,24 @@ def clean_array(board):  # Clean a array - 'C' Command.
             board[line][col] = "O"
     return board
 
-# TODO: Mover comentários para docstring.
-# TODO: Expandir o cmd em parâmetros.
-# TODO: Receber os parâmetros já no tipo correto.
-# TODO: Verificar se faz sentido implementar um objeto Coordenada/Cell/etc que encapsule a conversão de índices dos
-#       comandos em índices do board: board[Coord(1, 2)] = color -> __set_item__
-# TODO: Renomear colos_pixel para color.
 
-
-def color_pixel(cmd, board):  # Change the color of one pixel - 'L' Command.
+def color_pixel(cmd, board):
+    # TODO: Expandir o cmd em parâmetros.
+    # TODO: Receber os parâmetros já no tipo correto.
+    # TODO: Verificar se faz sentido implementar um objeto Coordenada/Cell/etc que encapsule a conversão de índices dos
+    #       comandos em índices do board: board[Coord(1, 2)] = color -> __set_item__
+    # TODO: Renomear colos_pixel para color.
+    """Change the color of one pixel - 'L' Command."""
     col, line, color = cmd
 
     board[int(line) - 1][int(col) - 1] = color
     return board
 
-# TODO: Mover comentário para docstring.
-# TODO: Renomear ver_pixel para algo como vertical_line.
-# TODO: Expandir cmd em parâmetros que devem ser recebidos já nos tipos corretos.
 
-
-def ver_pixel(cmd, board):  # Change the color of a column - 'V' Command.
+def ver_pixel(cmd, board):
+    """Change the color of a column - 'V' Command."""
+    # TODO: Renomear ver_pixel para algo como vertical_line.
+    # TODO: Expandir cmd em parâmetros que devem ser recebidos já nos tipos corretos.
     # TODO: Manter as variáveis alinhadas com o range. lineIni -> start e lineEnd -> stop
     # TODO: C é um nome ruim.
     col, lineIni, lineEnd, C = cmd
@@ -97,13 +58,13 @@ def ver_pixel(cmd, board):  # Change the color of a column - 'V' Command.
         board[int(ver)][int(col) - 1] = C
     return board
 
-# TODO: Mover comentário para docstring.
-# TODO: Renomear hor_pixel para algo como horizontal_line
-# TODO: Expandir cmd em parâmetros que devem ser recebidos já nos tipos corretos.
-# TODO: Verificar se é possível uniformizar a assinatura de ver_pixel e hor_pixel.
 
-
-def hor_pixel(cmd, board):  # Change the color of a line - 'H' Command.
+def hor_pixel(cmd, board):
+    """Change the color of a line - 'H' Command."""
+    # TODO: Mover comentário para docstring.
+    # TODO: Renomear hor_pixel para algo como horizontal_line
+    # TODO: Expandir cmd em parâmetros que devem ser recebidos já nos tipos corretos.
+    # TODO: Verificar se é possível uniformizar a assinatura de ver_pixel e hor_pixel.
     # TODO: Manter as variáveis alinhadas com o range. colIni -> start e colEnd -> stop
     colIni, colEnd, line, color = cmd
 
@@ -114,11 +75,12 @@ def hor_pixel(cmd, board):  # Change the color of a line - 'H' Command.
     return board
 
 
-# TODO: Mover comentários para docstring
-# TODO: Melhorar nome block_pixel
-# TODO: Expandir o cmd em parâmetros que devem ser recebidos já nos tipos corretos.
-# TODO: Uniformizar os parâmetros com o vertical_line e horizontal_line.
-def block_pixel(cmd, board):  # Change color of an entire block - 'K' Command.
+def block_pixel(cmd, board):
+    """Change color of an entire block - 'K' Command."""
+    # TODO: Melhorar nome block_pixel
+    # TODO: Expandir o cmd em parâmetros que devem ser recebidos já nos tipos corretos.
+    # TODO: Uniformizar os parâmetros com o vertical_line e horizontal_line.
+
     # TODO: Alinhar os nomes dos parâmetros como na função range.
     colIni, lineIni, colEnd, lineEnd, color = cmd
 
@@ -129,11 +91,11 @@ def block_pixel(cmd, board):  # Change color of an entire block - 'K' Command.
     return board
 
 
-# TODO: Ajustar a ordem de argumentos para respeitar a lógica de um par ordenado.
-# TODO: Mover comentário para docstring.
-# TODO: Melhorar o nome out_range, pois ele está muito atrelado ao detalhe de implementação com listas.
-# TODO: Verificar se essa função não poderia ser substituida pelo método __contains__ -> Coord(x, y) in board
-def out_range(board, Y, X):  # Check if a cmd is out of list range.
+def out_range(board, Y, X):
+    """Check if a cmd is out of list range."""
+    # TODO: Ajustar a ordem de argumentos para respeitar a lógica de um par ordenado.
+    # TODO: Melhorar o nome out_range, pois ele está muito atrelado ao detalhe de implementação com listas.
+    # TODO: Verificar se essa função não poderia ser substituida pelo método __contains__ -> Coord(x, y) in board
     # TODO: Renomar para width e height.
     line = len(board)
     col = len(board[0])
@@ -147,10 +109,11 @@ def out_range(board, Y, X):  # Check if a cmd is out of list range.
         return False
 
 
-# TODO: Melhorar o nome fill_pixel.
-# TODO: Expandir cmd para os parâmetros já com os tipos corretos.
-# TODO: Mover comentário para docstrings
-def fill_pixel(cmd, board):  # Fill a continuous region 'F' command.
+def fill_pixel(cmd, board):
+    """Fill a continuous region 'F' command."""
+    # TODO: Melhorar o nome fill_pixel.
+    # TODO: Expandir cmd para os parâmetros já com os tipos corretos.
+
     col, line, chgColor = cmd
 
     # TODO: Melhorar o nome da variável color para enfatizar que se trata de uma cor original/anterior.
@@ -185,16 +148,43 @@ def fill_pixel(cmd, board):  # Fill a continuous region 'F' command.
     return board
 
 
-# TODO: Mover comentários para uma docstring.
-# TODO: Melhorar o nome save_array.
-# TODO: Melhorar o nome do parâmetro name.
-# TODO: Utilizar a mesma lógica do "board as string" para persistir no arquivo texto.
-def save_array(name, board):  # Save the array with the 'S' command.
+def save_array(name, board):
+    """Save the array with the 'S' command."""
+    # TODO: Melhorar o nome save_array.
+    # TODO: Melhorar o nome do parâmetro name.
+    # TODO: Utilizar a mesma lógica do "board as string" para persistir no arquivo texto.
+
     # TODO: remover o lower.
     # TODO: melhorar o my_file para f.
     with open(name.lower(), "w") as my_file:
         for item in board:
             my_file.write("".join(item) + "\n")
+
+
+def read_sequence():
+    """Read and validate a sequence of commands."""
+    # TODO: Melhorar o nome read_sequence, pois na verdade ela lê o comando.
+    # TODO: Separar as responsabilidades do read_sequence para garantir a leitura de um comando válido e
+    #       validar o comando em si.
+    # TODO: Verificar se faz sentido ter um parser único para o comando validado, eliminando List[str] e usando algo
+    #       de mais alto nível.
+
+    # TODO: Melhorar o nome charValid para algo mais semântico.
+    # TODO: Remover os parêntesis desnecessários.
+    charValid = ("ICLVHKFSX")
+    # TODO: Melhorar o nome sqc. Sugestão: cmd
+    # TODO: Separar a obtenção do comando, do tratamento do comando.
+    sqc = input("Digite um comando: ").upper()
+    sqc = sqc.split()
+
+    for char in charValid:
+        # TODO: Verificar a real necessidade desse "and".
+        if char == sqc[0] and not "":
+            return sqc
+    else:
+        print("\nComando Inválido!\n")
+        return sqc
+    # TODO: Implementar um único return.
 
 
 def main():
